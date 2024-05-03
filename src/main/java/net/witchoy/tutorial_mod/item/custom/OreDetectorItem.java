cda73f25ecde80f0bd6b03817cd96402c104015d
Jules <julesgoy27@gmail.com>
2024-05-03 16:48:09 +0200

    Episode 10 of the tutorial finished !

--- src/main/java/net/witchoy/tutorial_mod/item/custom/OreDetectorItem.java
+++ src/main/java/net/witchoy/tutorial_mod/item/custom/OreDetectorItem.java
@@ -6,10 +6,16 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class OreDetectorItem extends Item {
     public OreDetectorItem(Properties properties) {
@@ -52,5 +58,11 @@ public class OreDetectorItem extends Item {
 
     private boolean isValuableBlock(BlockState state) {
         return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.tutorial_mod.metal_detector.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }
