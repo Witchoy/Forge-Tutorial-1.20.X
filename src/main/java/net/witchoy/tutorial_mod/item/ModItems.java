297e7a1fedcb906cf698f6bb4766e1f83622804c
Jules <julesgoy27@gmail.com>
2024-05-03 16:11:17 +0200

    Episode 6 of the tutorial finished !

--- src/main/java/net/witchoy/tutorial_mod/item/ModItems.java
+++ src/main/java/net/witchoy/tutorial_mod/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.witchoy.tutorial_mod.TutorialMod;
+import net.witchoy.tutorial_mod.item.custom.OreDetectorItem;
 
 public class ModItems {
     public static DeferredRegister<Item> ITEMS =
@@ -17,6 +18,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ORE_DETECTOR = ITEMS.register("ore_detector",
+            () -> new OreDetectorItem(new Item.Properties().durability(100)));
 
 
     public static void register(IEventBus eventBus) {
