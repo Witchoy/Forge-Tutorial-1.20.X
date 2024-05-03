2b88a76caddfe8df4e065017ba5cbaad7eaa8756
Jules <julesgoy27@gmail.com>
2024-05-03 14:23:41 +0200

    Episode 2 of the tutorial finished !

---
+++ src/main/java/net/witchoy/tutorial_mod/Items/ModCreativeModTabs.java
@@ -0,0 +1,30 @@
+package net.witchoy.tutorial_mod.Items;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.witchoy.tutorial_mod.TutorialMod;
+import org.checkerframework.checker.units.qual.C;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(()-> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems(((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+                    } ))
+
+                    .build());
+
+    public static void register(IEventBus bus) {
+        CREATIVE_MODE_TABS.register(bus);
+    }
+}
---
+++ src/main/java/net/witchoy/tutorial_mod/Items/ModItems.java
@@ -0,0 +1,26 @@
+package net.witchoy.tutorial_mod.Items;
+
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.witchoy.tutorial_mod.TutorialMod;
+
+public class ModItems {
+    public static DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
+
