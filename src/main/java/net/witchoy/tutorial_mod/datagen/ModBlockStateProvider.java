87def2daec870ec362be749ba10f238aeb4f52de
Jules <julesgoy27@gmail.com>
2024-05-04 10:20:08 +0200

    Episode 11 of the tutorial finished !

---
+++ src/main/java/net/witchoy/tutorial_mod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,30 @@
+package net.witchoy.tutorial_mod.datagen;
+
+import net.witchoy.tutorial_mod.TutorialMod;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.witchoy.tutorial_mod.block.ModBlocks;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.SAPPHIRE_BLOCK);
+        blockWithItem(ModBlocks.RAW_SAPPHIRE_BLOCK);
+
+        blockWithItem(ModBlocks.SAPPHIRE_ORE);
+        blockWithItem(ModBlocks.DEEPSLATE_SAPPHIRE_ORE);
+
+        blockWithItem(ModBlocks.SOUND_BLOCK);
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+}
